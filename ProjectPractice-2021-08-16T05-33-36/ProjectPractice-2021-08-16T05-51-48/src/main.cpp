5c3f3738553cff0e68343b7616a3857d3efa8c14
YXLopez <chavezlopezy@carleton.edu>
2021-09-19 02:50:29 -0700

    velocity + newest code
    set the mogo velocity at 75% of its max speed, added to void preauton to ensure it does not take time during user control. but if there are any issues it should be placed at void usercontrol instead right before while true statement

--- ProjectPractice-2021-08-16T05-33-36/ProjectPractice-2021-08-16T05-51-48/src/main.cpp
+++ ProjectPractice-2021-08-16T05-33-36/ProjectPractice-2021-08-16T05-51-48/src/main.cpp
@@ -36,7 +36,7 @@ competition Competition;
 void pre_auton(void) {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-
+  mogo.setVelocity(75, velocityUnits::pct);
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
 }
