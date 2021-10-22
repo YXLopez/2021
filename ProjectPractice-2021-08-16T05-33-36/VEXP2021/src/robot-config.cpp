f67515e6a8de79f69cc5bf6c4c8cba661bc70b78
YXLopez <chavezlopezy@carleton.edu>
2021-10-22 07:31:32 -0700

    before comp
    fixed this little shit

--- ProjectPractice-2021-08-16T05-33-36/VEXP2021/src/robot-config.cpp
+++ ProjectPractice-2021-08-16T05-33-36/VEXP2021/src/robot-config.cpp
@@ -8,8 +8,8 @@ using code = vision::code;
 brain  Brain;
 
 // VEXcode device constructors
-motor mogoMotorA = motor(PORT9, ratio36_1, true);
-motor mogoMotorB = motor(PORT2, ratio36_1, false);
+motor mogoMotorA = motor(PORT9, ratio18_1, true);
+motor mogoMotorB = motor(PORT2, ratio18_1, false);
 motor_group mogo = motor_group(mogoMotorA, mogoMotorB);
 controller Controller1 = controller(primary);
 motor RightDrive = motor(PORT1, ratio36_1, true);
